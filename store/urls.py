c99c0b0d2d707821fb0ac389352fb96cc69292be
Aditya Shekokar <adityashekokar1998@gmail.com>
2021-09-06 02:14:07 +0530

    Added base html and category filter

--- store/urls.py
+++ store/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from store import views
 
 urlpatterns = [
-    path("index/", views.index)
+    path("", views.index)
 ]
--- store/views.py
+++ store/views.py
@@ -2,8 +2,11 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-from store.models import Product
+from store.models import Product, Category
 
 
 def index(request):
-    return render(request, 'index.html', {"products": Product.objects.all()})
+    category_id = request.GET.get("category")
+    products = Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
+    return render(request, 'index.html', {"products": products,
+                                          "categories": Category.objects.all()})
